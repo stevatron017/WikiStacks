62c61adfa145eac905422482e696ddbbe4f39966
Noelle Laureano <nrlaureano@gmail.com>
2020-07-28 15:27:51 -0400

    updates model import in routes files

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const models = require("../models");
-const Page = models.Page;
-const User = models.User;
+const { Page, User } = require("../models");
 const { main, addPage, editPage, wikiPage } = require("../views");
 
 // /wiki
