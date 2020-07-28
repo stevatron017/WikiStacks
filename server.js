0d47783fccef96925d24e70b1df4de78062b270e
Noelle Laureano <nrlaureano@gmail.com>
2020-07-28 15:43:51 -0400

    tweaks .sync() comments

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const init = async () => {
   // Reference: https://sequelize.org/master/manual/model-basics.html#model-synchronization
   // `.sync()` creates the table in the database if it doesn't exist (and does nothing if it already exists)
   // We can do this two ways:
-  // 1. call `.sync()` on each individual Sequelize model (e.g. `User`, `Page`), or
-  // await Page.sync()
-  // await User.sync()
+  // 1. call `.sync()` on each individual Sequelize model (see commented out code below), or
+  // await Page.sync();
+  // await User.sync();
 
-  // 2. call `.sync()` on the entire Sequelize instance (`db`) since our models are defined on it (i.e. `db.define(...)`)
+  // 2. call `.sync()` on the entire Sequelize instance (e.g. `db`) since our models are defined on it (i.e. `db.define(...)`)
   await db.sync();
 
   app.listen(PORT, () => {
