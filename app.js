0de11563b011fa7941b432f6758db3c9225af298
Noelle Laureano <nrlaureano@gmail.com>
2020-07-28 15:34:48 -0400

    removes 500 error handling

--- app.js
+++ app.js
@@ -15,11 +15,4 @@ app.get("/", function (req, res) {
   res.redirect("/wiki/");
 });
 
-// Error catching endware
-app.use((err, req, res, next) => {
-  console.error(err, typeof next);
-  console.error(err.stack);
-  res.status(err.status || 500).send(err.message || "Internal server error.");
-});
-
 module.exports = app;
