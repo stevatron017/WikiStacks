e9fa1d8e7f38a1081116135468a10377aa1a5763
ericpkatz <ericpkatz@gmail.com>
2020-07-30 07:51:48 -0400

    use method override to call delete and put express routes

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ app.use(morgan("dev")); //logging middleware
 app.use(express.static(path.join(__dirname, "./public"))); //serving up static files (e.g. css files)
 app.use(express.urlencoded({ extended: false })); //parsing middleware for form input data
 app.use(express.json());
+app.use(require('method-override')('_method'));
+
+
 
 app.use("/wiki", require("./routes/wiki"));
 app.use("/users", require("./routes/users"));
--- views/editpage.js
+++ views/editpage.js
@@ -4,7 +4,7 @@ const layout = require("./layout");
 module.exports = (page, author) => layout(html`
   <h3>Edit a Page</h3>
   <hr>
-  <form method="POST" action="/wiki/${page.slug}">
+  <form method="POST" action="/wiki/${page.slug}?_method=PUT">
     <div class="form-group">
       <label for="name" class="col-sm-2 control-label">Author Name</label>
       <div class="col-sm-10">
