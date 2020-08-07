d60e89edbecc5438d6f50caf8ab2c58074a89956
jbracht <39741126+jbracht@users.noreply.github.com>
2020-08-06 20:12:29 -0400

    Remove reference to error file in exporting views

--- views/index.js
+++ views/index.js
@@ -1,9 +1,8 @@
 const addPage = require("./addpage");
 const editPage = require("./editpage");
-const error = require("./error");
 const main = require("./main");
 const userList = require("./userlist");
 const userPages = require("./userpages");
 const wikiPage = require("./wikipage");
 
-module.exports = { addPage, editPage, error, main, userList, userPages, wikiPage };
+module.exports = { addPage, editPage, main, userList, userPages, wikiPage };
